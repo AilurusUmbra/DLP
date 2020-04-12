325c814e7350a6b8072db44eca3cd4c931b73791
AilurusUmbra <c2525441.cs04@g2.nctu.edu.tw>
2020-04-12 23:26:15 +0800

    Add hw2 files

---
+++ lab2/DCN.py
@@ -0,0 +1,55 @@
+from functools import reduce
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class DeepConvNet(nn.Module):
+    def __init__(self, activation=None, deepconv=[25,50,100,200], dropout=0.5):
+        super(DeepConvNet, self).__init__()
+
+        if not activation:
+            activation = nn.ELU
+
+        self.deepconv = deepconv
+        self.conv0 = nn.Sequential(
+            nn.Conv2d(
+                1, deepconv[0], kernel_size=(1, 5),
+                stride=(1,1), padding=(0,0), bias=True
+            ),
+            nn.Conv2d(
+                deepconv[0], deepconv[0], kernel_size=(2,1),
+                stride=(1,1), padding=(0,0), bias=True
+            ),
+            nn.BatchNorm2d(deepconv[0]),
+            activation(),
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Dropout(p=dropout)
+        )
+
+        for idx in range(1, len(deepconv)):
+            setattr(self, 'conv'+str(idx), nn.Sequential(
+                nn.Conv2d(
+                    deepconv[idx-1], deepconv[idx], kernel_size=(1,5),
+                    stride=(1,1), padding=(0,0), bias=True
+                ),
+                nn.BatchNorm2d(deepconv[idx]),
+                activation(),
+                nn.MaxPool2d(kernel_size=(1, 2)),
+                nn.Dropout(p=dropout)
+            ))
+
+
+        flatten_size =  deepconv[-1] * reduce(
+            lambda x,_: round((x-4)/2), deepconv, 750)
+        self.classify = nn.Sequential(
+            nn.Linear(flatten_size, 2, bias=True),
+        )
+
+    def forward(self, x):
+        for i in range(len(self.deepconv)):
+            x = getattr(self, 'conv'+str(i))(x)
+        # flatten
+        x = x.view(-1, self.classify[0].in_features)
+        x = self.classify(x)
+        return x
+
