0fe859115111765a5bd6752d2e7baed3c0a6c827
AilurusUmbra <c2525441.cs04@g2.nctu.edu.tw>
2020-04-20 18:08:43 +0800

    Modify `main.py`

--- lab3/main.py
+++ lab3/main.py
@@ -6,23 +6,18 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.models
 from torchvision import datasets, transforms
-from torch.autograd.function import once_differentiable
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
 import seaborn as sns
-import pandas as pd
-import random
 from tqdm import tqdm
-from PIL import Image
-import PIL
 from dataloader import RetinopathyDataset
 from torch.utils.data import DataLoader
+
+from models import ResNetPretrain
+from utils import *
 
 sns.set_style("whitegrid")
 device = torch.device("cuda")
 
-torch.cuda.set_device(0)
-
+torch.cuda.set_device(1)
 
 def cal_acc(model, loader):
     correct = 0
@@ -36,48 +31,8 @@ def cal_acc(model, loader):
             preds.extend(pred)
             targets.extend(target.view_as(pred))
             correct += pred.eq(target.view_as(pred)).sum().item()
-    
+
     return (correct / len(loader.dataset)) * 100, targets, preds
-
-
-def plot_confusion_matrix(y_true, y_pred, title):
-    
-    # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-    classes = [0, 1, 2, 3, 4]
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    
-    fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-    ax.figure.colorbar(im, ax=ax)
-
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           xticklabels=classes, yticklabels=classes,
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    fmt = '.2f'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    fig.tight_layout()
-    plt.savefig(title + "_cfmatrx.png")
-    plt.clf()
-    plt.cla()
-    plt.close()
-    
-    return ax
 
 
 if __name__ == '__main__':
@@ -104,21 +59,21 @@ if __name__ == '__main__':
 #         model_names = ["Resnet50_pretrain_2", "Resnet50_2"]
         model_names = ["Resnet50_pretrain_23"]#, "Resnet50_2"]
 #         model_names = ["Resnet18_pretrain_2", "Resnet18_2"]
-        model_names = ['Resnet50', "Resnet18"]
+        model_names = ['Resnet18_2']#Resnet50_2']#, "Resnet18_2"]
         load_models = [False, False]
         
         for idx, model_name in enumerate(model_names):
             print(model_name)
-            if model_name == "Resnet18":
-                model = ResNet18().to(device)
+            if model_name == "Resnet18_2":
+                model = ResNetPretrain(18, pretrained=False).to(device)
                 if load_models[idx]:
                     model.load_state_dict(torch.load("./" + model_name + ".pth"))
-                iteration = 15
-            elif model_name == "Resnet50":
-                model = ResNet50().to(device)
+                iteration = 1
+            elif model_name == "Resnet50_2":
+                model = ResNetPretrain(50, pretrained=False).to(device)
                 if load_models[idx]:
                     model.load_state_dict(torch.load("./" + model_name + ".pth"))
-                iteration = 8
+                iteration = 1
             elif model_name == "Resnet18_pretrain_2":
                 if load_models[idx]:
                     model = ResNetPretrain(18, pretrained=False).to(device)
@@ -181,10 +136,13 @@ if __name__ == '__main__':
             plt.close()
     
     else:
-        model_names = ["./Resnet18_2.pth", "./Resnet50_2.pth", "./Resnet18_pretrain_3.pth", "Resnet50_pretrain_23_82.pth"]#"./Resnet50_pretrain_2.pth"]
-        models = [ResNet18().to(device), ResNet50().to(device), ResNetPretrain(18, pretrained=False).to(device), ResNetPretrain(50, pretrained=False).to(device)]
-#         model_names = ["./Resnet18.pth", "./Resnet18_pretrain.pth"]
-#         models = [ResNet18().to(device), ResNetPretrain(18, pretrained=False).to(device)]
+        model_names = ["./weight/Resnet18_2.pth", "./weight/Resnet50_2.pth", 
+                "./weight/Resnet18_pretrain_2.pth", "./weight/Resnet50_pretrain_23_82.pth"]#"./Resnet50_pretrain_2.pth"]
+        models = [ResNetPretrain(18, pretrained=False).to(device), 
+                ResNetPretrain(50, pretrained=False).to(device), 
+                ResNetPretrain(18, pretrained=False).to(device), 
+                ResNetPretrain(50, pretrained=False).to(device)]
+
         print("Testing")
         for idx, name in enumerate(model_names):
             print(name[2:-6])
